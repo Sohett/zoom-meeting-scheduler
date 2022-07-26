9bf1f7bd9c45b64fea00147839061ffb8c569ead
Thomas Sohet <sohet.thomas@gmail.com>
2022-07-26 22:50:16 +0200

    add a meeting creation API integration with Zoom

--- back/src/processor.js
+++ back/src/processor.js
@@ -1,6 +1,12 @@
 const axios = require('axios');
 
-const { ZOOM_AUTH_URI, ZOOM_CLIENT_ID, ZOOM_CLIENT_SECRET } = process.env;
+const { ZOOM_AUTH_URI, ZOOM_CLIENT_ID, ZOOM_CLIENT_SECRET, ZOOM_CREATE_MEETING_URI } = process.env;
+
+const postRequest = (config, payload = {}) => {
+  const options = { ...config, data: payload };
+
+  return axios(options);
+};
 
 const requestAccessToken = async () => {
   const config = {
@@ -12,14 +18,34 @@ const requestAccessToken = async () => {
     },
   };
 
-  const { data: { access_token } } = await axios(config);
+  const { data: { access_token } } = await postRequest(config);
   return access_token;
+};
+
+const createMeeting = async (accessToken, payload) => {
+  const config = {
+    method: 'POST',
+    url: ZOOM_CREATE_MEETING_URI,
+    headers: { Authorization: `Bearer ${accessToken}` },
+  };
+
+  const meetingParams = {
+    start_time: payload.meetingStart,
+    topic: payload.meetingTopic,
+    duration: payload.meetingDuration,
+    type: 2,
+    default_password: false,
+  };
+
+  const { data: { join_url } } = await postRequest(config, meetingParams);
+  return join_url;
 };
 
 const processRequest = async (payload) => {
   const accessToken = await requestAccessToken(payload);
-  console.log('[ACCESS TOKEN]', accessToken);
-  return { accessToken };
+  const meetingInformation = await createMeeting(accessToken, payload);
+  console.log('[MEETING INFO]', meetingInformation);
+  return { meetingInformation };
 };
 
 module.exports = { processRequest };
